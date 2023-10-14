5480b8fd07481105ac73c72425477b0df32e5abb
undonepotato <icyseas@outlook.com>
2023-10-13 21:32:02 -0500

    Add contact page, animations, and X for sidebar, cleaned up code

--- scripts/fit-size.js
+++ scripts/fit-size.js
@@ -8,16 +8,29 @@ the content and navigation to the screen.
 
 const hamburgerButton = document.querySelector("#hamburger-menu");
 const sideNavElements = document.querySelectorAll(".side-nav, #darken-not-sidebar");
+const sideNavExit = document.querySelector("#side-nav-back");
 const contentDarken = document.querySelector("#darken-not-sidebar");
 const rootElement = document.documentElement;
 
 hamburgerButton.addEventListener("click", toggleSidebar);
 
+for (element of sideNavElements) {
+    element,addEventListener("animationend", sidebarClosed);
+}
+
+sideNavExit.addEventListener("click", toggleSidebar)
+
 contentDarken.addEventListener("click", toggleSidebar);
+contentDarken.addEventListener("animationend", sidebarClosed);
 
 function toggleSidebar () {
     for (element of sideNavElements) {
-        element.classList.toggle("activated");
+        if (element.classList.contains("activated")) {
+            element.classList.add("closing");
+        }
+        else {
+            element.classList.add("activated");
+        }
     }
     if (rootElement.style.overflow == "hidden") {
         rootElement.style.overflow = "visible";
@@ -26,3 +39,13 @@ function toggleSidebar () {
         rootElement.style.overflow = "hidden";
     }
 }
+
+function sidebarClosed () {
+    for (element of sideNavElements) {
+        if (element.classList.contains("closing")) {
+            element.classList.remove("activated");
+            element.classList.remove("closing");
+        }
+    }
+}
+
